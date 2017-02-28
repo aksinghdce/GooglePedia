77a4c5bff0ec4e812c00afda53d230fb81734957
Amit Kumar Singh <aksinghdce@gmail.com>
2017-02-28 09:59:34 +0530

    added graphical display for trees

---
+++ Users/Amit/Checkpoints/10.Trees/Tree_Resources.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# I need to understand working with trees in a comprehensive manner. With so many resources available on the internet it becomes confusing whether the subject is finished or it has more gems to explore. 
+# 
+# This document is a collection of resources that would help build a thourough understanding of trees as a data structure.
+# 
+# Resource 1: Basic algorithms on Trees to understand the trees ecosystem in computer science. From any standard book. For me it's "Data Structure and Algorithmic Thinking with Python by Narsimha Karumanchi".
+# 
+# Resource 2: [Geeks for Geeks](http://www.geeksforgeeks.org/category/tree/)
+
+# # Resource 1
+# 
+# 
+
+# In[5]:
+
+float("-infinity")
+
+
+# In[14]:
+
+
+
+
+# In[15]:
+
+
+
+
+# In[16]:
+
+
+
